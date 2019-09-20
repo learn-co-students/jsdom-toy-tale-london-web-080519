cb467e7edbcfe4a6ef1ffc44e22f9301bf25802f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-20 11:57:22 +0100

    got patch to update db, but not dynamically

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let addToy = false
 
 // YOUR CODE HERE
 
+
 //FETCH
 //with response data, make div.card for each toy & append to #toy-collection
 const toyCollection = document.getElementById("toy-collection")
@@ -21,7 +22,9 @@ function addToyToCollection(toy) {
 
   let button = document.createElement('button')
   button.setAttribute("class", "like-btn")
+  button.setAttribute("id", toy['id'])
   button.innerText = "Like <3"
+  button.addEventListener("click", increaseLikes)
 
   let frame = document.createElement('div')
   frame.setAttribute("class", "card")
@@ -34,20 +37,44 @@ function addToyToCollection(toy) {
   toyCollection.appendChild(frame)
 }
 
-const likeButton = document.querySelectorAll("like-button")
+const url = "http://localhost:3000/toys"
+
+// 1. get the button
+const likeButton = document.querySelectorAll(".like-button")
+
+//patch request should pass in url id of toy and increase its id['likes'] by 1
+function increaseLikes(event) {
+  // let toySelected = event.target.parentNode.firstElementChild.innerText;
+  let id = event.target.id
+  return fetch(`${url}/${id}`, {
+    method: 'PATCH',
+    //mode: 'cors', 
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }, body: JSON.stringify({
+      "likes": "get elements id, increase by 1"
+    })
+  })
+ // .then(response=>response.json())
+}
 
 //When page loads, make a get request to fetch all toys
 document.addEventListener("DOMContentLoaded", function() {
-  fetch("http://localhost:3000/toys")
+  fetch(url)
     .then(response => response.json())
     .then(obj => {
       for (let i=0; i<obj.length; i++) {
           addToyToCollection(obj[i])
       }
     })
-
 })
 
+addBtn.addEventListener("click", addNewToy)
+
+function addNewToy(event) {
+ // event.target
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -61,3 +88,5 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+
